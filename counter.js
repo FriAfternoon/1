da16d143571b0636b85ff972c163fc1ce5809cf1
FriAfternoon <128879846+FriAfternoon@users.noreply.github.com>
2023-03-30 01:10:08 +0800

    change counter

--- counter.js
+++ counter.js
@@ -2,7 +2,7 @@ export function setupCounter(element) {
   let counter = 0
   const setCounter = (count) => {
     counter = count
-    element.innerHTML = `count change to ${counter}`
+    element.innerHTML = `count changes ${counter}`
   }
   element.addEventListener('click', () => setCounter(counter + 1))
   setCounter(0)
